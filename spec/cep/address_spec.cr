6c63a27ab6053697c8314c2c18cbda4bf82ace4e
Hugo Abonizio <hugo_abonizio@hotmail.com>
2018-05-09 13:16:29 -0300

    Merge cep.cr methods

---
+++ spec/cep/address_spec.cr
@@ -0,0 +1,29 @@
+require "../spec_helper"
+
+describe Burocracia::CEP::Address do
+  describe "#find" do
+    context "when the cep exists" do
+      it "returns an address" do
+        address = Burocracia::CEP::Address.find "80210130"
+        address.nil?.should be_false
+        address.street.blank?.should be_false
+      end
+    end
+
+    context "when a cep does not exist" do
+      it "raises Burocracia::CEP::ApiRequestError" do
+        expect_raises(Burocracia::CEP::ApiRequestError, /404/) do
+          Burocracia::CEP::Address.find "123"
+        end
+      end
+    end
+
+    context "when an integer is passed" do
+      it "returns an address" do
+        address = Burocracia::CEP::Address.find 80210130
+        address.nil?.should be_false
+        address.street.blank?.should be_false
+      end
+    end
+  end
+end
